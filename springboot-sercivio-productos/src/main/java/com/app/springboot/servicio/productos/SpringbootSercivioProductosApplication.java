48e893dd1de140b2fb805c4a1ca61660928e4f0c
Hector Avellaneda <ahectore@gmail.com>
2019-11-30 12:18:14 -0300

    primer commit

---
+++ springboot-sercivio-productos/src/main/java/com/app/springboot/servicio/productos/SpringbootSercivioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.app.springboot.servicio.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSercivioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSercivioProductosApplication.class, args);
+	}
+
+}
---
+++ springboot-sercivio-productos/src/test/java/com/app/springboot/servicio/productos/SpringbootSercivioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springboot.servicio.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSercivioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
